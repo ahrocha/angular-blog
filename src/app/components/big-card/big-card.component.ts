f2a5c7ae6ad27ee887407b66d648b6570f191061
Andrey Rocha <ar@tsl.io>
2023-10-14 21:43:22 -0300

    components using data from code, not placeholders.

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class BigCardComponent {
 
+  @Input()
+  photo: string = '';
+
+  @Input()
+  title: string = 'John Wick';
+
+  @Input()
+  description: string = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+  @Input()
+  url: string = 'https://www.imdb.com/title/tt2911666/';
+
+  @Input()
+  type: string = 'Movie';
 }
